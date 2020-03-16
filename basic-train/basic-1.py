98ea81e2468166b831e63aedeb44845aa4ebd3c9
MiCigo <micihaha@aliyun.com>
2020-03-16 23:11:36 +0800

    Create basic-1

---
+++ basic-train/basic-1.py
@@ -0,0 +1,27 @@
+'''
+@Description: code
+@Author: MiCi
+@Date: 2020-03-16 21:31:24
+@LastEditTime: 2020-03-16 22:00:13
+@LastEditors: MiCi
+'''
+
+
+import pandas as pd
+import numpy as np
+
+
+class Basic1(object):
+
+    def __init__(self):
+        return
+
+    def basic_use(self):
+        return
+
+
+if __name__ == '__main__':
+    print('Start Learn Numpy')
+    print('============================\n')
+    example = Basic1()
+    example.basic_use()
